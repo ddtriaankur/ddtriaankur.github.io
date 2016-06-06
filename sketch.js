af9e703134783b27876ad6ae66ab1dd945d48762
ddtriaankur <ddtriaankur@gmail.com>
2016-06-06 13:21:09 +0530

    sketch updated

--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
-var count = 0;
+var cat,button, line
+var number = 0;
+
 function setup() {
-  var cat = document.getElementById("cat");
-  cat.mousePressed(doSomething);
- createP('this is some text');
- 
+  createElement('h1', 'A Cat Clicker Web Application')
+  // createElement('p', "Click the cat image below and you will see the number of time with each increment." )
+  cat = createImg('https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426')
+  
+  button = createButton("Click here")
+  button.mousePressed(increment);
+  // cat.mousePressed(increment);
+	line = createP("Number of time you have clicked you cat is 0 " );
+
+
 }
 
-function doSomething(){
-  count++;
+function increment(){
+	line.html("Number of time you have clicked you cat is " + number++);
+
+}
+
+function draw() {
+
+if (button.mouseIsPressed){
+	console.log('asdkaskdjhaskdjhaskdhaskjh')
+}
+  
 }
